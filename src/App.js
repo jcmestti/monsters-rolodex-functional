0ed9fe23f9a449af2fb2cadfac3bdd835d082a12
Joao Carlos Mestti <jcmestti@limaconsulting.com>
2023-03-10 07:27:33 -0300

    App Scaffolding

--- src/App.js
+++ src/App.js
@@ -6,9 +6,36 @@ const App = () => {
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => setMonsters(users))
+  }, [])
+
+  const onSearchChange = event => {
+    const searchTerm = event.target.value.toLocaleLowerCase();
+    setSearchField(searchTerm);
+  }
+
+  const filteredMonsters = monsters.filter(monster => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+
   return (
     <div className="App">
       <h1 className="app-title"> Monster Rolodex </h1>
+
+      <input
+        onChange={onSearchChange}
+      />
+
+      <div>
+        {filteredMonsters.map(monster => {
+          return <div key={monster.id}>
+                  <h1> {monster.name} </h1>
+                </div>
+        })}
+      </div>
     </div>
   )
 
